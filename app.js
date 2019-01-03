a1f0375382c16359978ecc17a339ed473350f046
Sam Ennis <samennis127@gmail.com>
2019-01-03 18:48:49 +0000

    Files

---
+++ app.js
@@ -0,0 +1,113 @@
+const express = require("express")
+      ejs = require("ejs")
+      paypal = require("paypal-rest-sdk");
+      app = express()
+      items = []
+      fs = require("fs");
+      yaml = require("js-yaml")
+      mysql = require("mysql")
+      port = "5000";
+
+
+ var catalog = yaml.safeLoad(fs.readFileSync('./catalog.yaml', 'utf8'));
+
+
+      paypal.configure({
+        'mode': 'sandbox', //sandbox or live
+        'client_id': 'AfG1gJkXYLfYhrof2ckJNwgF8fZYvzDmWUPBxqeOyPLGwstPTzB_gUh94p_JtNi_iaT8vKv4d8WtyEIB',
+        'client_secret': 'ECsq7iqQ5XGaZ2iE_rx9jpVXo7guT3jwhw8AWtfuoQhQTUH71L3jgr-ZaudLfrljP_OZn818y-IOM_A0'
+      });
+
+app.set('view engine', "ejs");
+
+function createObject(name, price, description, id) {
+  let item = {
+    "intent": "sale",
+    "payer": {
+        "payment_method": "paypal"
+    },
+    "redirect_urls": {
+        "return_url": `http://localhost:3000/purchase/${id}`,
+        "cancel_url": "http://localhost:3000/cancel"
+    },
+    "transactions": [{
+        "item_list": {
+            "items": [{
+                "name": name,
+                "sku": "001",
+                "price": price,
+                "currency": "EUR",
+                "quantity": 1
+            }]
+        },
+        "amount": {
+            "currency": "EUR",
+            "total": price
+        },
+        "description": description
+    }]
+};
+
+      return item;
+}
+
+
+//Items
+
+
+// End
+
+app.get("/", (req,res) => res.render('index'));
+
+app.post("/pay", (req,res) => {
+  let id = req.query.id;
+  let item = catalog[id];
+  console.log(item);
+  let paypalitem = createObject(item.name, item.value, item.description, id)
+
+paypal.payment.create(paypalitem, function (error, payment) {
+    if (error) {
+        throw error;
+    } else {
+        for(let i = 0; i < payment.links.length; i++) {
+          if(payment.links[i].rel === "approval_url") {
+            res.redirect(payment.links[i].href);
+          }
+
+        }
+    }
+});
+})
+
+  app.get('/purchase/:id', function (req, res) {
+
+        const payerID = req.query.PayerID;
+        const paymentID = req.query.paymentId;
+        const id = req.params.id;
+  const execute_payment_json = {
+  "payer_id": payerID,
+  "transactions": [{
+      "amount": {
+          "currency": "EUR",
+          "total": `${catalog[id].value}`
+      }
+  }]
+};
+
+paypal.payment.execute(paymentID, execute_payment_json, function (error, payment) {
+  if (error) {
+      console.log(error.response);
+      throw error;
+  } else {
+      console.log(JSON.stringify(payment));
+  }
+});
+})
+
+
+
+app.get("/cancel", (req,res) => {
+  res.respond("Cancelled")
+})
+
+app.listen(port, () => console.log(`Server live on ${port}`))
---
+++ functionalapp.js
@@ -0,0 +1,113 @@
+const express = require("express")
+      ejs = require("ejs")
+      paypal = require("paypal-rest-sdk");
+      app = express()
+      items = []
+      fs = require("fs");
+      yaml = require("js-yaml")
+      mysql = require("mysql")
+      port = 5000;
+
+ var catalog = yaml.safeLoad(fs.readFileSync('./catalog.yaml', 'utf8'));
+ var credentials = yaml.safeLoad(fs.readFileSync("./credentials.yaml", "utf8"));
+
+
+      paypal.configure({
+        'mode': 'sandbox', //sandbox or live
+        'client_id': `${credentials.client_id}`,
+        'client_secret': `${credentials.client_secret}`
+      });
+
+app.set('view engine', "ejs");
+
+function createObject(name, price, description, id) {
+  let item = {
+    "intent": "sale",
+    "payer": {
+        "payment_method": "paypal"
+    },
+    "redirect_urls": {
+        "return_url": `http://thediscordexperts.com/purchase/${id}`,
+        "cancel_url": "http://thediscordexperst.com/cancel"
+    },
+    "transactions": [{
+        "item_list": {
+            "items": [{
+                "name": name,
+                "sku": "001",
+                "price": price,
+                "currency": "EUR",
+                "quantity": 1
+            }]
+        },
+        "amount": {
+            "currency": "EUR",
+            "total": price
+        },
+        "description": description
+    }]
+};
+
+      return item;
+}
+
+
+//Items
+
+
+// End
+
+app.get("/", (req,res) => res.render('index'));
+
+app.post("/pay/:id", (req,res) => {
+  let id = req.params.id;
+  let item = catalog[id];
+  console.log(item);
+  let paypalitem = createObject(item.name, item.value, item.description, id)
+
+paypal.payment.create(paypalitem, function (error, payment) {
+    if (error) {
+        throw error;
+    } else {
+        for(let i = 0; i < payment.links.length; i++) {
+          if(payment.links[i].rel === "approval_url") {
+            res.redirect(payment.links[i].href);
+          }
+
+        }
+    }
+});
+})
+
+  app.get('/purchase/:id', function (req, res) {
+
+        const payerID = req.query.PayerID;
+        const paymentID = req.query.paymentId;
+        const id = req.params.id;
+  const execute_payment_json = {
+  "payer_id": payerID,
+  "transactions": [{
+      "amount": {
+          "currency": "EUR",
+          "total": `${catalog[id].value}`
+      }
+  }]
+};
+
+paypal.payment.execute(paymentID, execute_payment_json, function (error, payment) {
+  if (error) {
+      console.log(error.response);
+      throw error;
+  } else {
+      console.log(JSON.stringify(payment));
+  }
+});
+})
+
+
+
+app.get("/cancel", (req,res) => {
+  res.respond("Cancelled")
+})
+
+app.listen(port, () => console.log(`Server Started on port ${port}`))
